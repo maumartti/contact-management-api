66129c2cac949adbaccfa3f509929c26b645d08b
maumartti <maumartti@gmail.com>
2023-11-10 21:09:38 -0300

    creation of structure and tables,migrations,seeders

---
+++ database/migrations/2023_11_10_225811_create_accounts_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('accounts', function (Blueprint $table) {
+            $table->id();
+            $table->string('name',20);
+            $table->string('email',36);
+            $table->string('address');
+            $table->string('image',22);
+            $table->string('tel',12);
+            $table->unsignedInteger('user_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('accounts');
+    }
+};
---
+++ database/seeders/UserTableSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\User;
+
+class UserTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $persons = [
+            ['name' => 'admin', 'email' => 'admin@gmail.com', 'password' => '11111111'],
+            ['name' => 'user2', 'email' => 'user2@gmail.com', 'password' => '22222222'],
+            ['name' => 'user3', 'email' => 'user3@gmail.com', 'password' => '33333333'],
+        ];
+
+        foreach ($persons as $item) {
+            User::create([
+                'name' => $item['name'],
+                'email' => $item['email'],
+                'password' => bcrypt('password'), // Coloca aquí la contraseña encriptada
+            ]);
+        }
+    }
+}
